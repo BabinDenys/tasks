1c4c6e06cfd4675a8a904ccd9bc5880a0c6ba5e8
Denys <denbabinis@gmail.com>
2019-04-19 18:49:35 +0200

    Первое задание

---
+++ 1_DZ/script.js
@@ -0,0 +1,19 @@
+
+var money = prompt("Ваш бюджет на месяц?");
+var time = prompt("Введите дату в формате YYYY-MM-DD");
+
+
+var appData = {
+    butget:money,
+    timeData:time,
+    expenses:{
+        answer1  :  answer2
+    },
+    optionalExpenses: {},
+    income: arr,
+    savings: false
+};
+var arr;
+var answer1 =  prompt("Введите обязательную статью расходов в этом месяце");
+var answer2 =  prompt("Во сколько обойдется?");
+alert(+money/30);
