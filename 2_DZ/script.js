c48ecb340cf2d7bf2d0897fea6ca5ca86000e9d4
Denys <denbabinis@gmail.com>
2019-04-20 12:00:03 +0200

    Второе задание

---
+++ 2_DZ/script.js
@@ -0,0 +1,20 @@
+// let x = 5; 
+// console.log(x++);
+
+//  console.log([]+false-null+true);
+// console.log( 0 || "" || 2 || undefined || true || falsе);
+// let y = 1;
+// let x=y=2;
+// console.log(([]+1));
+// console.log("12"[1]);
+// let x = false ;
+// if(!x && !x) {
+//     console.log(1);
+// }
+
+// let a = [1,2,3];
+// let b = [1,2,3];
+// console.log(a===b);
+// console.log( +"Infinity" );
+// console.log("а" > "б");
+// console.log("з" > "ж");
