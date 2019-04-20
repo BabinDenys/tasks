5d8df8397cd4f289baca80ac924e918f953555c2
Denys <denbabinis@gmail.com>
2019-04-20 12:59:51 +0200

    Усовершенствование первого задания

--- 1_DZ/script.js
+++ 1_DZ/script.js
@@ -1,19 +1,65 @@
 
-var money = prompt("Ваш бюджет на месяц?");
+var money = +prompt("Ваш бюджет на месяц?");
 var time = prompt("Введите дату в формате YYYY-MM-DD");
 
-
 var appData = {
-    butget:money,
-    timeData:time,
-    expenses:{
-        answer1  :  answer2
-    },
+    budget: money,
+    timeData: time,
+    expenses:{},
     optionalExpenses: {},
-    income: arr,
+    income: [],
     savings: false
 };
-var arr;
-var answer1 =  prompt("Введите обязательную статью расходов в этом месяце");
-var answer2 =  prompt("Во сколько обойдется?");
-alert(+money/30);
+
+for (let i = 0; i<2; i++) {
+    var a1 =  prompt("Введите обязательную статью расходов в этом месяце");
+    var a2 =  prompt("Во сколько обойдется?");
+    if ((typeof(a1)) === "string" && a1 != "" && a1.length < 50 &&
+        a2 != "" && typeof(a1) != null && typeof(a2) != null  ) {
+
+            appData.expenses[a1] = a2;
+    } else{
+        i--;
+    }
+}
+
+appData.moneyPerDay = appData.budget/30;
+if(appData.moneyPerDay <= 100) {
+    console.log("Прожиточный минимум");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay <= 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");   
+}
+
+alert( "Ежедневный бюджет: " + appData.moneyPerDay);
+
+
+// while (i<2) {
+//     i++;
+//     var a1 =  prompt("Введите обязательную статью расходов в этом месяце");
+//     var a2 =  prompt("Во сколько обойдется?");
+//     if ((typeof(a1)) === "string" && a1 != "" && a1.length < 50 &&
+//         a2 != "" && typeof(a1) != null && typeof(a2) != null  ) {
+
+//             appData.expenses[a1] = a2;
+//     } else{
+//         i--;
+//     }
+// }
+
+
+//  let i = 0;
+// do {
+//     i++;
+//     var a1 =  prompt("Введите обязательную статью расходов в этом месяце");
+//     var a2 =  prompt("Во сколько обойдется?");
+//     if ((typeof(a1)) === "string" && a1 != "" && a1.length < 50 &&
+//         a2 != "" && typeof(a1) != null && typeof(a2) != null  ) {
+
+//             appData.expenses[a1] = a2;
+//     } else{
+//         i--;
+//     }
+
+// } while(i<2);
